92b8addc1c80d8c7fb8a59b54e8acaa0ea90979b
Augu <joseyaugustine18@gmail.com>
2023-06-12 17:10:56 +0530

    quiz updated to add neighbours quiz too

--- src/pages/Geography.tsx
+++ src/pages/Geography.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Box, Center, Heading, Spinner } from "@chakra-ui/react";
 import { DataSet } from "../data/dataset";
 import Quiz from "../components/Quiz";
+import { AllowedCountries, AllowedStates, AllowedUts, StateObject } from "../data/states";
 
 const DistrictQuizGenerator = (districtsData?: Map<string, string[]> | null, utData?: Map<string, string[]> | null) => {
     const filterMapByArgument = (map: Map<string, string[]>, argument: string): string[] => {
@@ -84,9 +85,73 @@ const DistrictQuizGenerator = (districtsData?: Map<string, string[]> | null, utD
     }
 }
 
+const StatesQuizGenerator = (statesData: Map<AllowedStates | AllowedCountries | AllowedUts, StateObject> | null) => {
+    // each states has a states array, ut array, sea array, countries array
+    // 1) states neighbouring - yes and no  - 8 question in total.
+    if (statesData) {
+        const statesArray: Array<AllowedStates | AllowedCountries | AllowedUts> = Array.from(statesData.keys()).sort(() => Math.random() - 0.5).slice(0, 28);
+        const allNeighbours = () => {
+            const allValues: string[] = [];
+            statesData.forEach((stateObj) => {
+                const { states = [], sea = [], ut = [], countries = [] } = stateObj;
+                allValues.push(...states, ...sea, ...ut, ...countries);
+            });
+            const uniqueValues = Array.from(new Set(allValues));
+            return uniqueValues;
+        }
+        const getItemsByState = (state: AllowedStates | AllowedCountries | AllowedUts): string[] => {
+            const stateObj = statesData.get(state);
+            if (!stateObj) {
+                return [];
+            }
+
+            const stateValues: string[] = [
+                ...(stateObj.states || []),
+                ...(stateObj.sea || []),
+                ...(stateObj.ut || []),
+                ...(stateObj.countries || []),
+            ];
+
+            return stateValues;
+        }
+
+        const generateStateItems = (map: Map<string, StateObject>, states: Array<AllowedStates | AllowedCountries | AllowedUts>): DataSet[] => {
+            const output = [];
+            let i = 0;
+            do {
+                let state = states[i];
+                const stateObj = map.get(state);
+                const isOdd = i % 2 === 0;
+                if (stateObj) {
+
+                    const stateItems: string[] = getItemsByState(state).filter((item) => item != state);
+                    const notHasItems: string[] = allNeighbours().sort(() => Math.random() - 0.5).filter((item) => item != state).filter((item) => !stateItems.includes(item));
+
+                    const falseOptions = isOdd ? notHasItems.slice(0, 3) : stateItems.slice(0, 3);
+                    const actualAnswer = !isOdd ? notHasItems[0] : stateItems[0];
+                    falseOptions.push(actualAnswer);
+                    const options = falseOptions.sort(() => Math.random() - 0.5);
+                    const question = isOdd ? `Select one which does share border with ${state}` : `Select one which does not share border with ${state}`;
+                    const answer = options.indexOf(actualAnswer);
+                    options.length == 4 && output.push({ question, answer, options })
+                }
+                i++;
+            }
+            while (output.length < 21)
+            return output;
+        }
+
+        const stateItems = generateStateItems(statesData, statesArray);
+        return stateItems
+    }
+    // 2) generate a list of neighbours (may be some 4-5 random) and generate 4 options for neighbours list.
+
+}
+
 const Geography: React.FC = () => {
     const [districtsData, setDistrictsData] = useState<Map<string, string[]> | null>(null)
     const [utData, setUtData] = useState<Map<string, string[]> | null>(null)
+    const [statesData, setStatesData] = useState<Map<AllowedStates | AllowedCountries | AllowedUts, StateObject> | null>(null)
     const [districtsQuizData, setDistrictsQuizData] = useState<DataSet[] | null>(null)
 
     useEffect(() => {
@@ -100,12 +165,19 @@ const Geography: React.FC = () => {
                 setUtData(d.UTdistrictsMap);
             })
             .catch((error) => console.error(error));
+        import('../data/states')
+            .then((d) => {
+                setStatesData(d.default);
+            })
+            .catch((error) => console.error(error));
     }, [])
 
     useEffect(() => {
         const newQuizDataset = DistrictQuizGenerator(districtsData, utData)
         newQuizDataset && setDistrictsQuizData(newQuizDataset);
-    }, [districtsData])
+        const neighboursQuizSet = StatesQuizGenerator(statesData)
+        newQuizDataset && neighboursQuizSet && setDistrictsQuizData([...neighboursQuizSet,...newQuizDataset])
+    }, [districtsData, statesData])
     return <Box>
         <Center>
             <Heading>Geography Quiz</Heading>
