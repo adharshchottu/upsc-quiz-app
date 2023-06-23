e6c20a4d3bc7db571c41906861052a54dd9d3de7
Augu <joseyaugustine18@gmail.com>
2023-06-23 17:49:38 +0530

    districts and UT new question generator function

--- src/pages/Geography.tsx
+++ src/pages/Geography.tsx
@@ -5,68 +5,62 @@ import Quiz from "../components/Quiz";
 import { AllowedCountries, AllowedStates, AllowedUts, StateObject } from "../data/states";
 
 const DistrictQuizGenerator = (districtsData?: Map<string, string[]> | null, utData?: Map<string, string[]> | null) => {
-    const filterMapByArgument = (map: Map<string, string[]>, argument: string): string[] => {
-        const filteredDistricts: string[] = [];
-        map.forEach((districts, ut) => {
-            if (ut !== argument) {
-                filteredDistricts.push(...districts);
-            }
-        });
-        return filteredDistricts;
-    }
-
     if (districtsData && utData) {
-        const questionGenerator = (pairSet: Set<string[]>) => {
-            const statesArray = Array.from(districtsData.keys());
-            const quizData = Array.from(pairSet).map((d, i) => {
-                const filteredStates = statesArray.filter(state => state !== d[1]).sort(() => Math.random() - 0.5).slice(0, 3);
-                const filteredDistricts = filterMapByArgument(districtsData, d[1]).sort(() => Math.random() - 0.5).slice(0, 3);
+        const newQuestionGenerator = (pairSet: [string, string][]) => {
+            let i = 0;
+            const pairData = pairSet.sort(() => Math.random() - 0.5);
+            const quizData: DataSet[] = [];
+            while (i < 300) {
+                const questionPair = pairData[i]
                 const isOdd = i % 2 === 0;
-                const question = isOdd ? `Select a district in ${d[1]}` : `${d[0]} is in which state`;
-                const unShuffledOptions = !isOdd ? filteredStates : filteredDistricts;
-                unShuffledOptions[3] = isOdd ? d[0] : d[1]
-                const options = unShuffledOptions.sort(() => Math.random() - 0.5)
-                const answer = isOdd ? options.indexOf(d[0]) : options.indexOf(d[1])
-                return { question, options, answer }
-            })
+                const question = isOdd ? `Select a district in ${questionPair[0]}` : `${questionPair[1]} is in which state`;
+                const optionsPair = pairData.slice((i)).sort(() => Math.random() - 0.5).filter(d => d[0] != questionPair[0]).slice(0, 3)
+                const unSortedOptions = isOdd ? [...optionsPair.map(d => d[1]), questionPair[1]] : [...optionsPair.map(d => d[0]), questionPair[0]];
+                const unSortedOrderedOptionsPair = !isOdd ? [] : [...optionsPair, questionPair];
+                const rand = Math.random();
+                const options = unSortedOptions.sort(() => rand - 0.5)
+                const orderedOptionsPair = unSortedOrderedOptionsPair.sort(() => rand - 0.5)
+                const answer = isOdd ? options.indexOf(questionPair[1]) : options.indexOf(questionPair[0]);
+                if (options.length === new Set(options).size) {
+                    quizData.push({ question, answer, options, detailedAnswer: orderedOptionsPair })
+                }
+                i++;
+            }
             return quizData
         }
 
-        const questionGeneratorUT = (pairSetUT: Array<[string, string]>) => {
-            const UTsArray = Array.from(utData.keys());
-            const quizData = Array.from(pairSetUT).map((d, i) => {
-                const filteredStates = UTsArray.filter(state => state !== d[0]).sort(() => Math.random() - 0.5).slice(0, 3);
-                const filteredDistricts = filterMapByArgument(utData, d[0]).filter(state => state !== d[1]).sort(() => Math.random() - 0.5).slice(0, 3);
+        const newUTQuestionGenerator = (pairSet: [string, string][]) => {
+            let i = 0;
+            const pairData = pairSet.sort(() => Math.random() - 0.5);
+            const quizData: DataSet[] = [];
+            while (i < 52) {
+                const questionPair = pairData[i]
                 const isOdd = i % 2 === 0;
-                const question = isOdd ? `Select a place in ${d[0]}` : `${d[1]} is in which Union Territory`;
-                const unShuffledOptions = !isOdd ? filteredStates : filteredDistricts;
-                unShuffledOptions[3] = isOdd ? d[1] : d[0]
-                const options = unShuffledOptions.sort(() => Math.random() - 0.5)
-                const answer = isOdd ? options.indexOf(d[1]) : options.indexOf(d[0])
-                return { question, options, answer }
-            })
+                const question = isOdd ? `Select a place in ${questionPair[0]}` : `${questionPair[1]} is in which Union Territory`;
+                const optionsPair = pairData.slice((i)).sort(() => Math.random() - 0.5).filter(d => d[0] != questionPair[0]).slice(0, 3)
+                const unSortedOptions = isOdd ? [...optionsPair.map(d => d[1]), questionPair[1]] : [...optionsPair.map(d => d[0]), questionPair[0]];
+                const unSortedOrderedOptionsPair = !isOdd ? [] : [...optionsPair, questionPair];
+                const rand = Math.random();
+                const options = unSortedOptions.sort(() => rand - 0.5)
+                const orderedOptionsPair = unSortedOrderedOptionsPair.sort(() => rand - 0.5)
+                const answer = isOdd ? options.indexOf(questionPair[1]) : options.indexOf(questionPair[0]);
+                if (options.length === new Set(options).size) {
+                    quizData.push({ question, answer, options, detailedAnswer: orderedOptionsPair })
+                }
+                i++;
+            }
             return quizData
         }
 
         // districts
-
-        // 1. generate a unique set of 100 districts
-        // Create an empty set to store the pairs
-        const pairsSet: Set<string[]> = new Set();
-
-        // Loop until the set size is off all districts
-        while (pairsSet.size < 300) {
-            // Get a random state from the map keys
-            const states = [...districtsData.keys()];
-            const state = states[Math.floor(Math.random() * states.length)];
-
-            // Get a random district from the map values
-            const districts = districtsData.get(state);
-            const district = districts && districts[Math.floor(Math.random() * districts.length)];
-
-            // Add the pair to the set as a string
-            pairsSet.add([`${district}`, `${state}`]);
-        }
+        const districtsPair: Array<[string, string]> = [];
+        districtsData.forEach((districts, ut) => {
+            districts.forEach((district) => {
+                const utDistrict: [string, string] = [ut, district];
+                districtsPair.push(utDistrict);
+            });
+        });
+        
 
         // union territories
         const UTdistricts: Array<[string, string]> = [];
@@ -77,8 +71,8 @@ const DistrictQuizGenerator = (districtsData?: Map<string, string[]> | null, utD
             });
         });
 
-        const UTquizDataset = questionGeneratorUT(UTdistricts);
-        const DistrictquizDataset = questionGenerator(pairsSet);
+        const UTquizDataset = newUTQuestionGenerator(UTdistricts);
+        const DistrictquizDataset = newQuestionGenerator(districtsPair);
         const quizDataset = [...UTquizDataset, ...DistrictquizDataset].sort(() => Math.random() - 0.5)
         return quizDataset
 
@@ -176,7 +170,7 @@ const Geography: React.FC = () => {
         const newQuizDataset = DistrictQuizGenerator(districtsData, utData)
         newQuizDataset && setDistrictsQuizData(newQuizDataset);
         const neighboursQuizSet = StatesQuizGenerator(statesData)
-        newQuizDataset && neighboursQuizSet && setDistrictsQuizData([...neighboursQuizSet,...newQuizDataset])
+        newQuizDataset && neighboursQuizSet && setDistrictsQuizData([...neighboursQuizSet, ...newQuizDataset])
     }, [districtsData, statesData])
     return <Box>
         <Center>
