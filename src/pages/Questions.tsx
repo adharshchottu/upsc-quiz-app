9a3abc4a2393cbc012b0792e8f7cc17cd1bd8446
Augu <joseyaugustine18@gmail.com>
2024-07-13 18:08:15 +0530

    loading screen

--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -1,14 +1,16 @@
-import { Box, Button, Flex, Heading, Text, useToast } from "@chakra-ui/react";
+import { Button, Flex, Heading, IconButton, Spinner, Text, useToast } from "@chakra-ui/react";
 import { collection, getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { db } from "../firebase/firebase-app";
-import { customUserInfo } from "../auth/AuthContext";
+import { AuthContext, customUserInfo } from "../auth/AuthContext";
 import { optionsInterface } from "./Question";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 
 export interface questionInterface {
     id: string;
     question: string;
+    questionDirection: string;
     explanation: string;
     answer: string;
     options: optionsInterface;
@@ -19,15 +21,33 @@ export interface questionInterface {
 const AddQuestion = () => {
     const navigate = useNavigate();
     return <>
-        <Button onClick={() => {
-            navigate("/question")
-        }}>Add Question</Button>
+        <Button
+            onClick={() => {
+                navigate("/question")
+            }}
+            size={["sm", null, "md", "lg"]}
+        >Add Question</Button>
     </>
 }
 
+const Loading = () => {
+    return <Flex justifyContent={"center"} alignItems={"center"} h={'lg'} direction={"column"}>
+        <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+        />
+        <Text mt={2}>Fetching questions for you.</Text>
+    </Flex>
+}
+
 const Questions: React.FC = () => {
+    const navigate = useNavigate();
     const [questions, setQuestions] = useState<questionInterface[]>([])
     const toast = useToast()
+    const { user } = useContext(AuthContext);
 
     const FetchQuestions = async () => {
         try {
@@ -55,23 +75,46 @@ const Questions: React.FC = () => {
     }, [])
     return <>
         <Flex justifyContent={"space-evenly"}>
-            <Heading textAlign={"center"} color={"teal"}>Questions</Heading>
+            <Heading textAlign={"center"} color={"teal"} fontSize={["xl", "2xl", "3xl", "4xl"]}>Questions</Heading>
             <AddQuestion />
         </Flex>
-        <Flex direction={"column"} gap={4} mx={56} my={16}>
+        {questions.length ? <Flex direction={"column"} gap={4} maxW={"container.lg"} mx={[4, 8, 12, 16, "auto"]} my={[8, null, 12, null]}>
             {
-                questions.map((question,index) => {
-                    return <Box key={question.id}>
-                        <Text color={"grey.800"} fontSize={"lg"} fontWeight={"bold"}>
-                            {index+1}. {question.question}
-                        </Text>
-                        <Text color={"grey.400"} fontSize={"sm"} pl={4}>
-                            {question.options.optionOne}
-                        </Text>
-                    </Box>
+                questions.map((question, index) => {
+                    return <Flex justifyContent={"space-between"} alignItems={"center"} key={question.id} gap={"2"}>
+                        <Flex direction={"column"}>
+                            <Text color={"grey.800"} fontSize={["sm", null, "md", "xl"]} fontWeight={"bold"}>
+                                {index + 1}. {question.question}
+                            </Text>
+                            <Text color={"grey.400"} fontSize={"xs"} pl={[2, null]} fontStyle={"italic"}>
+                                {question.user.displayName}
+                            </Text>
+                        </Flex>
+                        {
+                            question.user.uid == user?.uid && <Flex gap={[3, null]}>
+                                <IconButton
+                                    size={["xs", null, "sm", "md"]}
+                                    variant='outline'
+                                    colorScheme='blue'
+                                    aria-label='Edit Question'
+                                    fontSize={['12px', null, null]}
+                                    icon={<EditIcon />}
+                                    onClick={() => navigate(`/question?id=${question.id}`)}
+                                />
+                                <IconButton
+                                    size={["xs", null, "sm", "md"]}
+                                    variant='outline'
+                                    colorScheme='red'
+                                    aria-label='Deleted Question'
+                                    fontSize={['12px', null, null]}
+                                    icon={<DeleteIcon />}
+                                />
+                            </Flex>
+                        }
+                    </Flex>
                 })
             }
-        </Flex>
+        </Flex> : <Loading />}
     </>
 }
 
